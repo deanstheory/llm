62f175fa124fd8c07e706136611da22881d5feaf
Deanstheory <sean.doherty1@gmail.com>
2025-05-04 20:22:24 +0100

    env key update

--- mylangflow.py
+++ mylangflow.py
@@ -3,7 +3,7 @@
 import os
 from mylangflow import load_flow_from_json
 
-os.environ["OPENAI_API_KEY"] = "my_key"
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', 'YourAPIKey')
 
 flow = load_flow_from_json("./langflow_pdf_loader.json")
 # Now you can use it like any chain
